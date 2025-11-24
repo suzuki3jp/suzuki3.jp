2538ea95e5b375e22737d1ccc70152d84e4eae4b
suzuki3jp <suzuki3.info@gmail.com>
2025-11-24 15:42:41 +0900

    chore: Footer の年を動的に

--- src/features/footer/index.tsx
+++ src/features/footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <CenteredLayout>
       <footer className="pb-5 text-center font-semibold text-[14px]">
-        © 2025 Built with ⌨️🖱️ by suzuki3jp
+        © {new Date().getFullYear()} Built with ⌨️🖱️ by suzuki3jp
       </footer>
     </CenteredLayout>
   );
